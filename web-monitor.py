7d3d0f05184cda116aaa66c28543c46759ded953
Yunong Jiang <jiangyntz@gmail.com>
2023-08-24 10:50:32 -0700

    Optimize webdriver page loading

--- web-monitor.py
+++ web-monitor.py
@@ -8,6 +8,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 from config import *
 from constants import *
@@ -19,10 +21,20 @@ def get_tesla_inventory_info(
     zip_code: str, model: ModelKey, driver: WebDriver
 ) -> List[WebElement]:
     driver.get(URL + model.value + QUERY + zip_code)
-    time.sleep(5)  # Let the page load
 
-    # Find the element(s) - Adjust the method to fit the actual webpage structure
-    inventory_elements = driver.find_elements(By.CSS_SELECTOR, "article.result.card")
+    try:
+        result_container: WebElement = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "div.results-container--has-results")
+            )
+        )
+        # Find the element(s) - Adjust the method to fit the actual webpage structure
+        inventory_elements: List[WebElement] = result_container.find_elements(
+            By.CSS_SELECTOR, "article.result.card"
+        )
+    except Exception as e:
+        print("Failed to find inventory elements. Error: {}".format(e))
+        return []
 
     return inventory_elements
 
@@ -112,53 +124,59 @@ if __name__ == "__main__":
 
     mode = "test" if args.test else "prod"
 
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    )
+
     while True:
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        options.add_argument(
-            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-        )
-        driver = webdriver.Chrome(options)
+        try:
+            driver = webdriver.Chrome(options)
+            clients = get_user_input_from_gs(INPUT_SHEET_KEY[mode])
+            existing_data_ids = set(get_data_ids_in_gs())
+            new_data_ids = set()
+            gs_data_to_write = []
 
-        clients = get_user_input_from_gs(INPUT_SHEET_KEY[mode])
-        existing_data_ids = set(get_data_ids_in_gs())
-        new_data_ids = set()
-        gs_data_to_write = []
-
-        for (zip_code, model), conditions in clients.items():
-            inventory_elements = get_tesla_inventory_info(zip_code, model, driver)
-            for condition in conditions:
-                cars = filter_tesla_inventory(
-                    inventory_elements,
-                    condition,
-                    model,
-                    zip_code,
-                    existing_data_ids,
-                )
-                if len(cars) > 0:
-                    send_email(
-                        "Tesla Availability Alert | "
-                        + MODEL_KEY_NAME_MAP[model].value
-                        + " | "
-                        + ZIPCODE_TO_AREA[zip_code],
-                        condition["email"],
-                        ("\n\n\n").join(
-                            [format_email_content(car_info) for car_info in cars]
-                        ),
+            for (zip_code, model), conditions in clients.items():
+                inventory_elements = get_tesla_inventory_info(zip_code, model, driver)
+                for condition in conditions:
+                    cars = filter_tesla_inventory(
+                        inventory_elements,
+                        condition,
+                        model,
+                        zip_code,
+                        existing_data_ids,
                     )
+                    if len(cars) > 0:
+                        send_email(
+                            "Tesla Availability Alert | "
+                            + MODEL_KEY_NAME_MAP[model].value
+                            + " | "
+                            + ZIPCODE_TO_AREA[zip_code],
+                            condition["email"],
+                            ("\n\n\n").join(
+                                [format_email_content(car_info) for car_info in cars]
+                            ),
+                        )
 
-                    for car in cars:
-                        if car.data_id not in new_data_ids:
-                            new_data_ids.add(car.data_id)
-                            gs_data_to_write.append(car.to_gs_row())
+                        for car in cars:
+                            if car.data_id not in new_data_ids:
+                                new_data_ids.add(car.data_id)
+                                gs_data_to_write.append(car.to_gs_row())
 
-                    if args.playsound:
-                        from playsound import playsound
+                        if args.playsound:
+                            from playsound import playsound
 
-                        playsound("./alert.mp3")
+                            playsound("./alert.mp3")
 
-        if len(gs_data_to_write) > 0:
-            write_to_gs(gs_data_to_write)
+            if len(gs_data_to_write) > 0:
+                write_to_gs(gs_data_to_write)
 
-        driver.quit()
-        time.sleep(random.randint(60, 120))
+            driver.quit()
+            time.sleep(random.randint(60, 120))
+
+        except KeyboardInterrupt:
+            print("Quitting driver...")
+            driver.quit()
+            break
